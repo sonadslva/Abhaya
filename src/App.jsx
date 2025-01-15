d9634daeb792dc454070bda71bdf19c9ef5cc528
sona dsilva <sonadsilva2000@gmail.com>
2025-01-15 17:31:48 +0530

    allcompleted1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
-import Login from './components/login';
+import Login from './components/Login';
 import Home from './components/Home';
 import AboutUs from './components/About';
 
